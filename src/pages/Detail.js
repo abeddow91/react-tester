afe6c6cc4a444bb8f18ea88474f230d915fbff45
Anna Beddow <anna.beddow@gmail.com>
2017-01-25 19:39:34 +0000

    added superagent and github ajax call

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,27 +1,41 @@
 import React from 'react';
-import Chance from 'chance';
+//you can use whatever you want instead of ajax, such as request.
+import ajax from 'superagent';
 
 class Detail extends React.Component {
   constructor(props){
     super(props);
 
-    const people = []
+    this.state = {
+      commits: []
+     };
+  }
 
-    for (let i = 0; i<10; i++) {
-      people.push({
-        name: chance.first(),
-        country: chance.country({full: true}),
-      });
-    }
 
-    this.state = { people };
+//n.b SuperAgent code is asynchronous so the anonymous function will only run nce it is finished getting github's response
+//Also, response.body is supoerageny magic - it has detected that github responded with content type "application/json" and automatically converted githubs response into javascript objects which is why we can sent response.body straight into our state, as it is already a javascript array of objects ready to use. 
+// is the name of the method and needs to be named exactly this in order for React to call it.
+  componentWillMount() {
+    //tells SuperAgent to fetch the list of commits to the React project from Github
+    ajax.get('https://api.github.com/repos/facebook/react/commits')
+    //tells SuperAgent what to do when the request finishes it should run the anonymous function that follows
+        .end((error, response) => {
+          // starts a conditional statement: the following should run only when there is no error and there was a response from the server.
+          if (!error && response) {
+          // updates our component's state using the body value of the SuperAgent response
+            this.setState({commits: response.body});
+          } else {
+            console.log('There was an error fetching from GitHub', error);
+          }
+        }
+      );
   }
 
   render() {
     return (
       <div>
-      {this.state.people.map((person, index) => (
-        <p key={index}>Hello, {person.name} from {person.country}!.</p>
+      {this.state.commits.map((commit, index) => (
+        <p key={index}>some data here.</p>
       ))}
       </div>);
   }
