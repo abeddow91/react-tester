9e5e4ef91180096f4670535ab14c8498b337438a
Anna Beddow <anna.beddow@gmail.com>
2017-01-25 19:22:03 +0000

    added map loop test

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -4,39 +4,25 @@ import Chance from 'chance';
 class Detail extends React.Component {
   constructor(props){
     super(props);
-    this.state = {
-      name: chance.first(),
-      country: chance.country({full: true}),
-      clicks: 0
-    };
-  }
 
-  countClick(){
-    this.setState({
-      clicks: this.state.clicks + 1
-    });
-  }
-  checkName() {
-    if (this.state.name === "John"){
-      return "Oh, Hi John";
+    const people = []
+
+    for (let i = 0; i<10; i++) {
+      people.push({
+        name: chance.first(),
+        country: chance.country({full: true}),
+      });
     }
-    else {
-      return "oh, sorry, I thought you were John";
-    }
-  }
-  buttonClicked() {
-    this.forceUpdate();
+
+    this.state = { people };
   }
 
   render() {
     return (
       <div>
-       <p>{this.checkName()}!</p>
-        <p>Hello, {this.state.name}.</p>
-        <p>Youre from {this.state.country}.</p>
-        <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
-        <p>there has been {this.state.clicks} clicks</p>
-        <button onClick={this.countClick}>Add to clicks</button>
+      {this.state.people.map((person, index) => (
+        <p key={index}>Hello, {person.name} from {person.country}!.</p>
+      ))}
       </div>);
   }
 }
