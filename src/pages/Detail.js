986acd2f0de9acbe31b58457c4b0ac2bb55c3f81
Anna Beddow <anna.beddow@gmail.com>
2017-01-25 18:52:22 +0000

    added click button

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -2,18 +2,42 @@ import React from 'react';
 import Chance from 'chance';
 
 class Detail extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: chance.first(),
+      country: chance.country({full: true}),
+      clicks: 0
+    };
+  }
+
+  countClick(){
+    this.setState({
+      clicks: this.state.clicks + 1
+    });
+  }
   checkName() {
-    if (chance.first() === "John"){
+    if (this.state.name === "John"){
       return "Oh, Hi John";
     }
     else {
       return "oh, sorry, I thought you were John";
     }
   }
+  buttonClicked() {
+    this.forceUpdate();
+  }
+
   render() {
-    return(
-      <p>{this.checkName()}!</p>
-    );
+    return (
+      <div>
+       <p>{this.checkName()}!</p>
+        <p>Hello, {this.state.name}.</p>
+        <p>Youre from {this.state.country}.</p>
+        <button onClick={this.buttonClicked.bind(this)}>Meet Someone New</button>
+        <p>there has been {this.state.clicks} clicks</p>
+        <button onClick={this.countClick}>Add to clicks</button>
+      </div>);
   }
 }
 
