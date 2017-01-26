f24664c4050098e47f1e4a735cae8ae5a0a17b9b
Anna Beddow <anna.beddow@gmail.com>
2017-01-26 14:51:50 +0000

    added details to user page

--- src/pages/User.js
+++ src/pages/User.js
@@ -6,7 +6,7 @@ class User extends React.Component {
   constructor(){
     super();
     this.state ={
-      user:[]
+      details:[]
     };
 
   }
@@ -14,16 +14,27 @@ componentWillMount(){
   ajax.get(`https://api.github.com/users/${this.props.params.username}/events`)
       .end((error, response) => {
         if (!error && response) {
-          this.setState({user: response.body})
+          this.setState({details: response.body})
+          console.log("test test test");
+          console.log(this.state.details);
         } else {
           console.log("Sorry, there was an error pulling the user data from GitHub.")
         }
       });
 }
-
+renderUser(){
+  return this.state.details.map((user,index)=>(
+    <div key={index}>
+      <strong>{user.payload.push_id}</strong>
+    </div>
+  ));
+}
 
   render(){
-    return <p>some text user</p>
+    return (<div>
+      <p>Page for {this.props.params.username} </p>
+      {this.renderUser()}
+      </div>);
   }
 }
 
