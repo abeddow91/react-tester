799dd157de2144d81b9ca6c51622473f051faf05
Anna Beddow <anna.beddow@gmail.com>
2017-01-26 11:13:30 +0000

    refactored the ajax call

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -17,40 +17,29 @@ class Detail extends React.Component {
 //n.b SuperAgent code is asynchronous so the anonymous function will only run nce it is finished getting github's response
 //Also, response.body is supoerageny magic - it has detected that github responded with content type "application/json" and automatically converted githubs response into javascript objects which is why we can sent response.body straight into our state, as it is already a javascript array of objects ready to use.
 // is the name of the method and needs to be named exactly this in order for React to call it.
-  componentWillMount() {
-    //tells SuperAgent to fetch the list of commits to the React project from Github
-    ajax.get('https://api.github.com/repos/facebook/react/commits')
-    //tells SuperAgent what to do when the request finishes it should run the anonymous function that follows
-        .end((error, response) => {
-          // starts a conditional statement: the following should run only when there is no error and there was a response from the server.
-          if (!error && response) {
-          // updates our component's state using the body value of the SuperAgent response
-            this.setState({commits: response.body});
-          } else {
-            console.log('There was an error fetching from GitHub', error);
-          }
-        }
-      );
-      ajax.get('https://api.github.com/repos/facebook/react/pulls')
-          .end((error, response) => {
-            if (!error && response) {
-              this.setState({pulls: response.body});
-            } else {
-              console.log('There was an error fetching from GitHub', error);
-            }
-          }
-        );
 
-      ajax.get('https://api.github.com/repos/facebook/react/forks')
-          .end((error, response) => {
-            if (!error && response) {
-              this.setState({forks: response.body});
-            } else {
-              console.log('There was an error fetching from GitHub', error);
-            }
-          }
-        );
+fetchFeed(type){
+  //   //tells SuperAgent to fetch the list of commits to the React project from Github
+  ajax.get(`https://api.github.com/repos/facebook/react/${type}`)
+  //   //tells SuperAgent what to do when the request finishes it should run the anonymous function that follows
+      .end((error,response) => {
+  // starts a conditional statement: the following should run only when there is no error and there was a response from the server.
+        if (!error && response) {
+// updates our component's state using the body value of the SuperAgent response
+          this.setState({ [type]: response.body} );
+        } else{
+          console.log(`There was an error fetching ${type} from GitHub`, error);
+        }
+    });
+}
+
+
+  componentWillMount() {
+    this.fetchFeed('commits');
+    this.fetchFeed('pulls');
+    this.fetchFeed('forks');
   }
+
   showCommits(){
     this.setState({ mode: "commits"});
   }
